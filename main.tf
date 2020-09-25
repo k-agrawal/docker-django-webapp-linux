35bb71f2e0b5e7580bf8fcdcaa0cc1bf5eee1717
Kanishk Agrawal <kanishk120@outlook.com>
2020-09-25 16:28:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "azurerm" {
+  
+  }
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "docker-appserviceplan"
+  location            = "eastus"
+  resource_group_name = "con-tf-ex-rg"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "docker-app-service"
+  location            = "eastus"
+  resource_group_name = "con-tf-ex-rg"
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
